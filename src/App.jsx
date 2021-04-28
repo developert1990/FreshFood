e6068bb42449e58c3f03776deafb10db4f40dd01
developert1990 <developert.hong@gmail.com>
2021-04-28 03:29:35 -0600

    added footer and data json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Footer } from './components/Footer.jsx';
 
 function App() {
     return (
         <div className="App">
-            Base
+            <Footer />
         </div>
     );
 }
