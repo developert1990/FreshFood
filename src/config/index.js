79e9993267d53aee021e04c1e70ce958e58f7f01
developert1990 <developert.hong@gmail.com>
2021-08-06 16:20:11 -0600

    asdf

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,4 @@ const EC2 = process.env.REACT_APP_EC2;
 export const EMAIL_REG_EXP = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 export const ZIPCODE_REG_EXP = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
 export const NAME_REG_EXP = /^[a-zA-Z]+$/;
-export const API = process.env.NODE_ENV === 'development' ? LOCAL : EC2;
+export const API = process.env.NODE_ENV === 'production' ? EC2 : LOCAL;
